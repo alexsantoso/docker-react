0f8367fce4e9e4fcdf1ab77b53ab2b07759bd8e4
Alex Santoso <alexsantoso@gmail.com>
2020-06-06 19:35:04 -0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>This is an edit by Alex - will it keep working? Well will it? Yaaay!</p>
+        <p>I was changed on the feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
